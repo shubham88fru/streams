b966f0222da9ffd5356d8ed7944e19412f201ecd
shubham88fru <shubham88fru@yahoo.com>
2021-06-06 11:44:03 +0530

    progrmmatic routing

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -8,6 +8,7 @@ import {
   EDIT_STREAM,
 } from './types';
 import streams from '../apis/streams';
+import history from '../history';
 
 export const signIn = (userId) => {
   return {
@@ -27,6 +28,7 @@ export const createStream = (formValues) => async (dispatch, getState) => {
   const response = await streams.post('/streams', { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  history.push('/');
 };
 
 export const fetchStreams = () => async (dispatch) => {
--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -8,11 +8,13 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
-  renderAdmin(steam) {
-    if (steam.userId === this.props.currentUserId) {
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
       return (
         <div className="right floated content">
-          <button className="ui button primary">Edit</button>
+          <Link to={`/streams/edit/${stream.id}`} className="ui button primary">
+            Edit
+          </Link>
           <button className="ui button negative">Delete</button>
         </div>
       );
